ffe95c3ffcda31c6819db325e232f5c0f1e40896
Nick Rameau <r4meau@gmail.com>
2016-10-09 02:04:08 -0700

    Fixed the safe browsing issue

--- abDetector.js
+++ abDetector.js
@@ -1,6 +1,6 @@
 /**
  * ==============
- *	 abDetector.js
+ *   abDetector.js
  * ==============
  *
  * @author: Nick Rameau
@@ -13,26 +13,68 @@
 window.onload = function() {
 	var iframe = document.createElement('iframe'),
 		randomNum = Math.floor(Math.random() * (10000 - 123 + 1)) + 123,
-		iframeLoaded = true,
 		protocol = window.location.protocol;
 
-	iframe.src = protocol +"//google.com/"+ randomNum + "/ads.html";
+	/*
+	*
+	* Creates a fake iframe, with a fake random url
+	* an id and gives it the minimum size
+	*
+	*/
+	iframe.src = protocol +"//google.com/"+ randomNum +"/ads.html";
 	iframe.height = ".1px";
 	iframe.width = ".1px";
 	iframe.id = 'some-ad';
-	iframe.onload = function() {iframeLoaded = false;};
 
-	document.body.appendChild(iframe);
+	/*
+	*
+	* Appends the iframe to the body only if it loaded successfully
+	* or the request got aborted due to some error inside the iframe (404)
+	*
+	*/
+	getRequest(iframe, function (response) { // On Success
+		document.body.appendChild(iframe);
+	}, function (xhr, status) { // On error
+		if (status === 0) // Request aborted
+	  		document.body.appendChild(iframe);
+	});
 
+	/*
+	*
+	* Takes 500 ms to run the code
+	* Selects the iframe from the body
+	* Runs some tests to know if it's being hidden or not
+	*
+	*/
 	setTimeout(function() {
 		var someAd = document.getElementById('some-ad');
-		if(!iframeLoaded ||
-		   someAd == null ||
-		   someAd.style.display == "none" ||
-		   someAd.style.display == "hidden" ||
-		   someAd.style.visibility == "hidden" ||
-		   someAd.offsetHeight == 0)
-	    	document.getElementById('ab-message').style.display = 'block';
-	    someAd.remove();
+		if(someAd === null ||
+		someAd.style.display == "none" ||
+		someAd.style.display == "hidden" ||
+		someAd.style.visibility == "hidden" ||
+		someAd.offsetHeight === 0)
+			document.getElementById('ab-message').style.display = 'block';
+		else
+			someAd.remove();
 	}, 500);
 };
+
+/*
+*
+* Uses XmlHttpRequest to load the iframe
+* Takes a success and an error function
+* and calls them respectively
+*
+*/
+function getRequest (iframe, success, error) {
+	var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+	xhr.open("GET", iframe.src);
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState == 4)
+			if (xhr.status == 200) // Loaded successfully
+				success(xhr.responseText);
+			else // For any other error
+				error(xhr, xhr.status);
+	};
+	xhr.send();
+}
